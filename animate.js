8af0e6932cb533ba4c9bfbeb45ac0d3b8a38f908
Paul Brzeski <mail@paulbrzeski.com>
2017-09-13 00:22:06 +0800

    Need to get these calculations right

--- animate.js
+++ animate.js
@@ -1,8 +1,8 @@
 console.log('\x1Bc');
 
 var twirlTimer = (function(logo) {
-  var frames = 4;
-  var frame = 0;
+  var frames = 6;
+  var frame = 1;
   var current_logo = logo;
   var fish1 = {
     topLeft: {
@@ -20,7 +20,7 @@ var twirlTimer = (function(logo) {
   };
   return setInterval(function() {
     current_logo = moveFish(fish1, frame, frames, logo);
-    //console.log('\x1Bc', "\x1b[36m",'\x1b[0m',"\r" + current_logo);
+    console.log('\x1Bc', "\x1b[36m",'\x1b[0m',"\r" + current_logo);
     frame++;
     frame &= (frames-1);
   }, 250);
@@ -29,27 +29,30 @@ var twirlTimer = (function(logo) {
 function moveFish(fish, frame, frames, logo) {
   var current_logo = logo;
   var lines = logo.split("\n");
+  var diff_line = ((fish.topLeft.line - fish.end.line) / (frames))* frame;
+  var diff_column = ((fish.topLeft.column - fish.end.column) / (frames))* frame;
   var current_position = {
     topLeft: {
-      line: parseInt(fish.topLeft.line - ((fish.topLeft.line - fish.end.line) / (frames) * frame )),
-      column: parseInt(fish.topLeft.column - ((fish.topLeft.column - fish.end.column) / (frames) * frame ))
+      line: parseInt(fish.topLeft.line - diff_line),
+      column: parseInt(fish.topLeft.column - diff_column)
     },
     bottomRight: {
-      line: parseInt(fish.bottomRight.line - ((fish.topLeft.line - fish.end.line) / (frames) * frame )),
-      column: parseInt(fish.bottomRight.column - ((fish.topLeft.column - fish.end.column) / (frames) * frame ))
+      line: parseInt(fish.bottomRight.line - diff_line),
+      column: parseInt(fish.bottomRight.column - diff_column)
     }
   };
 
   lines.forEach(function(line, number){
-    for (var i = 0; i < line.length; i++) {
-      if (i ) {
+    if (number >= current_position.topLeft.line && number <= current_position.bottomRight.line) {
+      line = line.substr(0, current_position.topLeft.column - diff_column) + line.substr(current_position.topLeft.column, line.length);
+      line = line.substr(0, current_position.bottomRight.column) + "M".repeat(diff_column) + line.substr(current_position.bottomRight.column, line.length);
+      lines[number] = line;
 
-      }
+      console.log(line);
     }
-    console.log(current_position);
-    //console.log(number, line);
   });
-  return current_logo;
+
+  return lines.join('');
 }
 
 fs = require('fs');

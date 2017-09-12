9e091fed7ea8ac47873ed7286861bfe34362049b
Paul Brzeski <mail@paulbrzeski.com>
2017-09-13 00:51:34 +0800

    Working on animation

--- animate.js
+++ animate.js
@@ -22,33 +22,37 @@ var twirlTimer = (function(logo) {
     current_logo = moveFish(fish1, frame, frames, logo);
     console.log('\x1Bc', "\x1b[36m",'\x1b[0m',"\r" + current_logo);
     frame++;
-    frame &= (frames-1);
+    if (frame >= frames) {
+      frame = 1;
+    }
   }, 250);
 });
 
 function moveFish(fish, frame, frames, logo) {
   var current_logo = logo;
   var lines = logo.split("\n");
-  var diff_line = ((fish.topLeft.line - fish.end.line) / (frames))* frame;
-  var diff_column = ((fish.topLeft.column - fish.end.column) / (frames))* frame;
+  var diff_line = Math.ceil((fish.topLeft.line - fish.end.line) / (frames));
+  var diff_column = Math.ceil((fish.topLeft.column - fish.end.column) / (frames));
   var current_position = {
     topLeft: {
-      line: parseInt(fish.topLeft.line - diff_line),
-      column: parseInt(fish.topLeft.column - diff_column)
+      line: parseInt(fish.topLeft.line - diff_line * frame),
+      column: parseInt(fish.topLeft.column - diff_column * frame)
     },
     bottomRight: {
-      line: parseInt(fish.bottomRight.line - diff_line),
-      column: parseInt(fish.bottomRight.column - diff_column)
+      line: parseInt(fish.bottomRight.line - diff_line * frame),
+      column: parseInt(fish.bottomRight.column - diff_column * frame)
     }
   };
-
+  console.log(frame);
+  console.log(current_position);
   lines.forEach(function(line, number){
     if (number >= current_position.topLeft.line && number <= current_position.bottomRight.line) {
-      line = line.substr(0, current_position.topLeft.column - diff_column) + line.substr(current_position.topLeft.column, line.length);
-      line = line.substr(0, current_position.bottomRight.column) + "M".repeat(diff_column) + line.substr(current_position.bottomRight.column, line.length);
+      line = line.substr(0, current_position.topLeft.column - diff_column) + line.substr(current_position.topLeft.column);
+      line = line.substr(0, current_position.bottomRight.column) + "M".repeat(diff_column) + line.substr(current_position.bottomRight.column);
       lines[number] = line;
-
       console.log(line);
+      
+      
     }
   });
 
